7d59cd932b896f301b9ad440a421c8918c2da375
Brad Cornes <hello@bradley.dev>
2022-04-27 11:59:57 +0100

    Remove firefox tests

--- playwright.config.js
+++ playwright.config.js
@@ -9,10 +9,6 @@ module.exports = {
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
